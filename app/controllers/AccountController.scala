659f3f6806894c5d4313afebdd9df36ef77d111d
YoshinoriN <nayo-msoft@outlook.jp>
2017-06-06 00:54:40 +0900

    Create AccountController

---
+++ app/controllers/AccountController.scala
@@ -0,0 +1,28 @@
+package app.controllers
+
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.i18n.{MessagesApi, I18nSupport}
+import play.api.db.slick._
+import slick.driver.JdbcProfile
+import app.models.{Account}
+import javax.inject.Inject
+import scala.concurrent.Future
+import slick.driver.H2Driver.api._
+
+class AccountController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
+                                  val messagesApi: MessagesApi) extends Controller
+    with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
+
+  def list = TODO
+
+  def edit(id: Option[Long]) = TODO
+
+  def create = TODO
+
+  def update = TODO
+
+  def remove(id: Long) = TODO
+}
