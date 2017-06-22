b662a1483d538997574c3ea167cdc17705c7d371
YoshinoriN <nayo-msoft@outlook.jp>
2017-06-22 23:58:03 +0900

    Remove Option type on Account table's created_at field and set current timestamp

--- app/models/Account.scala
+++ app/models/Account.scala
@@ -1,5 +1,8 @@
 package app.models
 
+import java.sql.Timestamp
+import java.util.Date
+
 import slick.driver.MySQLDriver.api._
 
 class Accounts(tag: Tag) extends Table[Account](tag, "account") {
@@ -7,7 +10,7 @@ class Accounts(tag: Tag) extends Table[Account](tag, "account") {
   val userName: Rep[String] = column[String]("user_name", O.Length(255,varying=true))
   val password: Rep[String] = column[String]("password", O.Length(255,varying=true))
   val isAdmin: Rep[Option[Boolean]] = column[Option[Boolean]]("is_admin", O.Default(Some(false)))
-  val createdAt: Rep[Option[java.sql.Timestamp]] = column[Option[java.sql.Timestamp]]("created_at")
+  val createdAt: Rep[java.sql.Timestamp] = column[java.sql.Timestamp]("created_at")
   val updatedAt: Rep[Option[java.sql.Timestamp]] = column[Option[java.sql.Timestamp]]("updated_at", O.Default(None))
   val deletedAt: Rep[Option[java.sql.Timestamp]] = column[Option[java.sql.Timestamp]]("deleted_at", O.Default(None))
   def * = (id, userName, password, isAdmin, createdAt, updatedAt, deletedAt) <> (Account.tupled, Account.unapply)
@@ -18,7 +21,7 @@ case class Account(
   userName: String,
   password: String,
   isAdmin: Option[Boolean] = Some(false),
-  createdAt: Option[java.sql.Timestamp],
+  createdAt: java.sql.Timestamp = new java.sql.Timestamp(System.currentTimeMillis()),
   updatedAt: Option[java.sql.Timestamp] = None,
   deletedAt: Option[java.sql.Timestamp] = None
 )
